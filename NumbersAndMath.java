dfd1cac419c54fd7362270ba75b6796abf7dff6f
Akhil Nair <akhiln007@gmail.com>
2016-08-23 11:32:09 +0100

    Mathematical operators

---
+++ NumbersAndMath.java
@@ -0,0 +1,21 @@
+public class NumbersAndMath
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Mathematial operators in Java");
+        
+        System.out.println("Hens" + (25+30/5));
+        System.out.println("Roosters" + (100-25*3%4));
+        
+        System.out.println("Is it ture that 3+2<5-7?");
+        System.out.println(3+2<5-7);
+        
+        System.out.println("What is 3+2\t" +(3+2));
+        System.out.println("What is 5-7\t" +(5-7));
+        
+        System.out.println("Is it greater\t" + (5>-2));
+        System.out.println("Is it greater or equal\t" + (5 >= -2));
+        System.out.println("Is it leass or equal\t" + (5 <= -2));
+    
+    }
+}
