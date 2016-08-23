c379c5d9d0bbf5ce6afbb37befdc3e8439188ae4
Akhil Nair <akhiln007@gmail.com>
2016-08-23 11:06:59 +0100

    Comments in java

---
+++ CommentsAndSlashes.java
@@ -0,0 +1,11 @@
+public class CommentsAndSlashes{
+    public static void main(String[] args)
+    {
+        //A comment, this is so you can read your program later.
+        System.out.println("Program about comments and slashes");
+        
+        //you can also use a comment to disable or comment out a piece of code.
+        //System.out.println("This won't run");
+        System.out.println("This will run");
+    }
+}
